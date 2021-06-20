e975824df08802d35d8d37e2acd34e109fc2d63f
KaitoSuzuki-0830 <kaito.083097@gmail.com>
2021-06-20 12:58:53 +0900

    Update Gemfile for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # ルート設定　application controller内の def helloというメソッドを指定
+  root 'application#hello'
 end
